1b7db6ac9451c42eeaba71342be586029e9db7b3
Akira Tamamori <tamamori5917@gmail.com>
2022-05-25 21:22:41 +0900

    Update get_data.py

--- scripts/data_generator/get_data.py
+++ scripts/data_generator/get_data.py
@@ -1,14 +1,21 @@
-import csv
-import youtube_dl as yt
 import argparse
+import csv
 import os
 import shutil
 
+import youtube_dl as yt
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-url_csv", required=True,
-                    help="CSV file where the url of the videos to download are.")
-parser.add_argument("-dataset_path", required=True,
-                    help="Output folder of the dataset divided into /video and /audio.")
+parser.add_argument(
+    "-url_csv",
+    required=True,
+    help="CSV file where the url of the videos to download are.",
+)
+parser.add_argument(
+    "-dataset_path",
+    required=True,
+    help="Output folder of the dataset divided into /video and /audio.",
+)
 args = parser.parse_args()
 
 
@@ -18,10 +25,15 @@ def read_channels(file_path):
     with open(file_path) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            parsed_channels.append({"name": row["Name"],
-                                    "gender": row["Gender"],
-                                    "url": row["Channel-URL"]})
+            parsed_channels.append(
+                {
+                    "name": row["Name"],
+                    "gender": row["Gender"],
+                    "url": row["Channel-URL"],
+                }
+            )
     return parsed_channels
+
 
 def download(url_list):
     error_counter = 0
@@ -29,10 +41,14 @@ def download(url_list):
 
     for url in url_list:
         count += 1
-        print "Downloading videos and audios {}/{} with url [{}] from {}".format(count, len(url_list), url['url'], url['name'])
-        out_path = os.path.join(args.dataset_path, url['name'])
-        video_out_path = os.path.join(out_path, 'video')
-        audio_out_path = os.path.join(out_path, 'audio')
+        print(
+            "Downloading videos and audios {}/{} with url [{}] from {}".format(
+                count, len(url_list), url["url"], url["name"]
+            )
+        )
+        out_path = os.path.join(args.dataset_path, url["name"])
+        video_out_path = os.path.join(out_path, "video")
+        audio_out_path = os.path.join(out_path, "audio")
         if not os.path.exists(out_path):
             os.makedirs(out_path)
             os.makedirs(video_out_path)
@@ -40,34 +56,31 @@ def download(url_list):
 
         try:
             video_options = {
-                'format': "135, 140",
-                'verbose': True,
-                'continuedl': True,
-                'ignoreerrors': True,
-                'nooverwrites': True,
-                'sleep_interval': 5,
-                'playliststart': 1,
-                'playlistend': 15,
-
+                "format": "135, 140",
+                "verbose": True,
+                "continuedl": True,
+                "ignoreerrors": True,
+                "nooverwrites": True,
+                "sleep_interval": 5,
+                "playliststart": 1,
+                "playlistend": 15,
             }
 
             with yt.YoutubeDL(video_options) as video_ydl:
-                video_ydl.download([url['url']])
-
+                video_ydl.download([url["url"]])
 
         except Exception:
-            print "Download error."
+            print("Download error.")
             error_counter += 1
 
-
         # ffmpeg -i input_file -ss 00:00:15.00 -t 00:00:10.00 -c copy out.mp4
-        workdir = os.listdir('./')
+        workdir = os.listdir("./")
         for files in workdir:
             if files.endswith(".mp4"):
                 shutil.move(files, video_out_path)
             elif files.endswith(".m4a"):
                 shutil.move(files, audio_out_path)
-    print "Found {} errors".format(error_counter)
+    print("Found {} errors".format(error_counter))
 
 
 urls = read_channels(args.url_csv)
