20d47a8b435c7c42538d12130719580e5da63bf5
Akira Tamamori <tamamori5917@gmail.com>
2022-05-25 21:25:06 +0900

    Update generator.py

--- models/generator.py
+++ models/generator.py
@@ -1,8 +1,9 @@
 import torch.nn as nn
+
 from models.auxiliary_classifier import auxclassifier
-#from models.segan.segan_discriminator import Discriminator
 from models.segan import Discriminator
 from models.spectral_norm import SpectralNorm
+
 
 class generator(nn.Module):
     def __init__(self, image_size, audio_samples):
@@ -17,55 +18,74 @@ class generator(nn.Module):
 
         # defining segan's D
         self.d_fmaps = [16, 32, 128, 256, 512, 1024]
-        self.audio_embedding = Discriminator(1, self.d_fmaps, 15, nn.LeakyReLU(0.3), self.audio_samples)
+        self.audio_embedding = Discriminator(
+            1, self.d_fmaps, 15, nn.LeakyReLU(0.3), self.audio_samples
+        )
         # defining the auxiliary classifier
         self.aux_classifier = auxclassifier()
 
         # common network for both architectures when generating 64x64 or 128x18 images
         self.netG = nn.Sequential(
             # state size. (ngf*4) x 8 x 8
-            SpectralNorm(nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False)),
+            SpectralNorm(
+                nn.ConvTranspose2d(self.ngf * 8, self.ngf * 4, 4, 2, 1, bias=False)
+            ),
             nn.Dropout(),
             # nn.BatchNorm2d(self.ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            SpectralNorm(nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False)),
+            SpectralNorm(
+                nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False)
+            ),
             # nn.BatchNorm2d(self.ngf),
             nn.Dropout(),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            SpectralNorm(nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1, bias=False)),
+            SpectralNorm(
+                nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1, bias=False)
+            ),
             nn.Dropout(),
             nn.ReLU(True),
             # If we add here Dropout, we would only generate noise, but not realistic faces
-            SpectralNorm(nn.ConvTranspose2d(self.ngf, self.num_channels, 4, 2, 1, bias=False)),
+            SpectralNorm(
+                nn.ConvTranspose2d(self.ngf, self.num_channels, 4, 2, 1, bias=False)
+            ),
             # state size. (num_channels) x 128 x 128
-            nn.Tanh()
+            nn.Tanh(),
         )
 
         # if we want to generate 64x64 images:
         if self.image_size == 64:
             self.netG = nn.Sequential(
-            SpectralNorm(nn.ConvTranspose2d(self.latent_dim, self.ngf*8, 4, 1, 0, bias=False)),
-            nn.Dropout(),
-            # nn.BatchNorm2d(self.ngf * 4),
-            nn.ReLU(True),
-            self.netG
+                SpectralNorm(
+                    nn.ConvTranspose2d(
+                        self.latent_dim, self.ngf * 8, 4, 1, 0, bias=False
+                    )
+                ),
+                nn.Dropout(),
+                # nn.BatchNorm2d(self.ngf * 4),
+                nn.ReLU(True),
+                self.netG,
             )
 
         # if we want to generate 128 x 128 images:
         if self.image_size == 128:
             self.netG = nn.Sequential(
-                SpectralNorm(nn.ConvTranspose2d(self.latent_dim, self.ngf*16, 4, 1, 0, bias=False)),
+                SpectralNorm(
+                    nn.ConvTranspose2d(
+                        self.latent_dim, self.ngf * 16, 4, 1, 0, bias=False
+                    )
+                ),
                 nn.Dropout(),
                 nn.ReLU(True),
-                SpectralNorm(nn.ConvTranspose2d(self.ngf*16, self.ngf*8, 4, 2, 1, bias=False)),
+                SpectralNorm(
+                    nn.ConvTranspose2d(self.ngf * 16, self.ngf * 8, 4, 2, 1, bias=False)
+                ),
                 nn.Dropout(),
                 # nn.BatchNorm2d(self.ngf * 4),
                 nn.ReLU(True),
-                self.netG
+                self.netG,
             )
-
 
     def forward(self, raw_wav):
 
@@ -80,4 +100,3 @@ class generator(nn.Module):
         output = self.netG(z_vector)
 
         return output, z_vector, softmax_scores
-
